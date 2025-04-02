76719e30020ac54deb4a57a8f21c80377e6bcf80
Yui <94931996+EdithKA@users.noreply.github.com>
2025-04-02 18:24:36 +0200

    Animacion Enemigos

--- Assets/Scripts/Characters/Enemies/AngleToPlayer.cs
+++ Assets/Scripts/Characters/Enemies/AngleToPlayer.cs
@@ -10,13 +10,16 @@ public class AngleToPlayer : MonoBehaviour
     Vector3 targetPos;
     Vector3 targetDir;
 
-    public float angle;
-    public int lastIndex:
+    float angle;
+    public int lastIndex;
+
+    SpriteRenderer spriteRenderer;
 
     // Start is called before the first frame update
     void Start()
     {
         player = FindObjectOfType<PlayerMove>().transform;
+        spriteRenderer= GetComponentInChildren<SpriteRenderer>();
     }
 
     // Update is called once per frame
@@ -27,9 +30,40 @@ public class AngleToPlayer : MonoBehaviour
 
         angle = Vector3.SignedAngle(targetDir, transform.forward, Vector3.up);
 
-        
+        //Voltear el sprite si es necesario
+        Vector3 tempScale = Vector3.one;
+        if(angle > 0)
+        {
+            tempScale.x = -1f;
+        }
+        lastIndex = GetIndex(angle);
     }
 
+    int GetIndex(float angle)
+    {
+
+        //Front
+        if (angle > -22.5f && angle < 22.6f)
+            return 0;
+        if (angle >= 22.5f && angle < 67.5f)
+            return 7;
+        if (angle >= 67.5f && angle < 112.5f)
+            return 6;
+        if (angle >= 112.5f && angle < 157.5f)
+            return 5;
+
+        //back
+        if (angle <= -157.5f || angle >= 157.5f)
+            return 4;
+        if (angle >= -157.4f && angle < -112.5f)
+            return 3;
+        if (angle >= -112.5f && angle < -67.5f)
+            return 2;
+        if (angle >= 67.5f && angle <= -22.5f)
+            return 1;
+
+        return lastIndex;
+    }
     private void OnDrawGizmosSelected()
     {
         Gizmos.color = Color.green;
--- Assets/Scripts/Characters/Enemies/EnemyController.cs
+++ Assets/Scripts/Characters/Enemies/EnemyController.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.AI;
 
@@ -10,36 +11,61 @@ public class EnemyController : MonoBehaviour
     private NavMeshAgent enemyNavMeshAgent;
     public float attackDistance;
 
+    // Animation
+    public Animator anim;
+    AngleToPlayer angleToPlayer;
+
+    // Patrol
+    public Transform[] waypoints; // Array de waypoints
+    private int currentWaypointIndex = 0;
+
     private void Start()
     {
         playerTransform = FindObjectOfType<PlayerMove>().transform;
         enemyNavMeshAgent = GetComponent<NavMeshAgent>();
+        angleToPlayer = GetComponent<AngleToPlayer>();
+
+        if (waypoints.Length > 0)
+        {
+            enemyNavMeshAgent.SetDestination(waypoints[currentWaypointIndex].position);
+        }
     }
 
     private void Update()
     {
         isAggressive = CheckPlayerDistance();
 
-        SetDestination();
+        anim.SetFloat("spriteRot", angleToPlayer.lastIndex);
+
+        if (isAggressive)
+        {
+            SetDestinationToPlayer();
+        }
+        else
+        {
+            Patrol();
+        }
     }
 
     bool CheckPlayerDistance()
     {
         var dist = Vector3.Distance(transform.position, playerTransform.position);
-        
         return dist <= attackDistance;
     }
 
-    void SetDestination()
+    void SetDestinationToPlayer()
     {
-        if(isAggressive)
-        {
-            enemyNavMeshAgent.SetDestination(playerTransform.position);
-        }
-        else
-        {
-            enemyNavMeshAgent.SetDestination(transform.position);
-        }
+        enemyNavMeshAgent.SetDestination(playerTransform.position);
+    }
 
+    void Patrol()
+    {
+        if (waypoints.Length == 0) return;
+
+        if (!enemyNavMeshAgent.pathPending && enemyNavMeshAgent.remainingDistance < 0.5f)
+        {
+            currentWaypointIndex = (currentWaypointIndex + 1) % waypoints.Length;
+            enemyNavMeshAgent.SetDestination(waypoints[currentWaypointIndex].position);
+        }
     }
 }
